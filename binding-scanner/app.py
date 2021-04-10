b79c3c3fe2f77b35d1b7811b6ec4364536365c49
krish8484 <akrish136@gmail.com>
2021-04-10 18:07:37 +0530

    ADD dcoker code

--- binding-scanner/app.py
+++ binding-scanner/app.py
@@ -2,4 +2,4 @@ from foca.foca import foca
 
 if __name__ == '__main__':
     app = foca("config.yaml")
-    app.run()
+    app.run(port=app.port)
--- binding-scanner/controllers.py
+++ binding-scanner/controllers.py
@@ -1,4 +1,5 @@
 import logging
+import subprocess
 
 from flask import (current_app, make_response)
 from werkzeug.exceptions import NotFound
@@ -12,81 +13,17 @@ error_response = {
 
 
 def TsvGet(id):
-    try:
-        db_collection = (
-            current_app.config['FOCA'].db.dbs['petstore']
-            .collections['pets'].client
-        )
-        filter_dict = {} if tags is None else {'tag': {'$in': tags}}
-        if not limit:
-            limit = 0
-        records = db_collection.find(
-            filter_dict,
-            {'_id': False}
-        ).sort([('$natural', -1)]).limit(limit)
-        return list(records)
-    except Exception as e:
-        logger.error(f"{type(e).__name__}: {e}")
-        return error_response
+    process2 = subprocess.Popen(['python3', '-m','pytest'], cwd="/app/weskit",
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE)
+    a, b = process2.communicate()
+    return str(a)
 
+def statusGet(id):
+    return "Hello3"
 
-def addPet(pet):
-    try:
-        db_collection = (
-            current_app.config['FOCA'].db.dbs['petstore']
-            .collections['pets'].client
-        )
-        counter = 0
-        ctr = db_collection.find({}).sort([('$natural', -1)])
-        if not ctr.count() == 0:
-            counter = ctr[0].get('id') + 1
-        record = {
-            "id": counter,
-            "name": pet['name'],
-            "tag": pet['tag']
-        }
-        db_collection.insert_one(record)
-        del record['_id']
-        return record
-    except Exception as e:
-        logger.error(f"{type(e).__name__}: {e}")
-        return error_response
+def HeatmapGet(id):
+    return "Hello"
 
-
-def findPetById(id):
-    try:
-        db_collection = (
-            current_app.config['FOCA'].db.dbs['petstore']
-            .collections['pets'].client
-        )
-        record = db_collection.find_one(
-            {"id": id},
-            {'_id': False},
-        )
-        if not record:
-            raise NotFound
-        return record
-    except Exception as e:
-        logger.error(f"{type(e).__name__}: {e}")
-        return error_response
-
-
-def deletePet(id):
-    try:
-        db_collection = (
-            current_app.config['FOCA'].db.dbs['petstore']
-            .collections['pets'].client
-        )
-        record = db_collection.find_one(
-            {"id": id},
-            {'_id': False},
-        )
-        if not record:
-            raise NotFound
-        db_collection.delete_one({"id": id})
-        response = make_response('', 204)
-        response.mimetype = current_app.config['JSONIFY_MIMETYPE']
-        return response
-    except Exception as e:
-        logger.error(f"{type(e).__name__}: {e}")
-        return error_response
+def infoPost(id):
+    return "Hello"
